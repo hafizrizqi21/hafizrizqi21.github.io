4bfed0dab4001c9ca75ee25d346ef731ae8c80b6
hafizh <hafiz.rizqi@gmail.com>
2019-07-19 17:31:32 +0700

    Updates

---
+++ precache-manifest.f8a981a1ce2eecec4e7e33ea0e325411.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2973afe2df31e783590604c88c070ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "260e4f93aaecd046d287",
+    "url": "/static/css/main.478eb32a.chunk.css"
+  },
+  {
+    "revision": "9403f0062c9f36dfc3ec",
+    "url": "/static/js/2.6e08da32.chunk.js"
+  },
+  {
+    "revision": "260e4f93aaecd046d287",
+    "url": "/static/js/main.919cdce2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
